3ad3bd8cb475d50aec3cfd7620c95a170ec23599
Suyunmeng <Susus0175@proton.me>
2026-03-22 15:41:50 +0800

    fix: update version to 3.0.4-beta in configuration and scripts

--- vite-frontend/src/config/site.ts
+++ vite-frontend/src/config/site.ts
@@ -4,7 +4,7 @@ export type SiteConfig = typeof siteConfig;
 
 // 缓存相关常量
 const CACHE_PREFIX = 'vite_config_';
-const VERSION = "3.0.3-beta";
+const VERSION = "3.0.4-beta";
 const APP_VERSION = "1.0.3";
 
 const getInitialConfig = () => {
